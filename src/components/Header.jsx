c886ac4b25738f7f1f422c9a0a88739ed0fa965c
Diego Moraes Cezar <dmcezar@gmail.com>
2022-09-18 16:40:44 -0300

    Adding SASS files and a Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header () {
+  return(
+    <div>
+      <h1>Senac Games Showcase</h1>
+      <h3>Amostra de jogos do curso de Game Design do Senac RJ</h3>
+    </div>
+  )
+}
