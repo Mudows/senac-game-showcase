a9067f0a62c92a716c42bdb71a77cd7a41ffe631
Diego Moraes Cezar <dmcezar@gmail.com>
2022-09-18 17:17:55 -0300

    ADD: Base Content with GameCard placeholder.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
 
 export default function Content() {
-  return(
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia pharetra neque, ut blandit sapien laoreet quis. Integer et tincidunt eros. Nam dolor odio, aliquet ac urna vitae, lobortis sagittis eros. Sed imperdiet sit amet est a molestie. Nunc consequat neque sed mauris convallis, sed ultrices massa eleifend. Proin ut consectetur est. Nulla facilisi. Duis eu enim urna. Curabitur et ultrices leo.</p>
-  )
+  return (
+    <div className="page-content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia
+        pharetra neque, ut blandit sapien laoreet quis. Integer et tincidunt
+        eros. Nam dolor odio, aliquet ac urna vitae, lobortis sagittis eros. Sed
+        imperdiet sit amet est a molestie. Nunc consequat neque sed mauris
+        convallis, sed ultrices massa eleifend. Proin ut consectetur est. Nulla
+        facilisi. Duis eu enim urna. Curabitur et ultrices leo.
+      </p>
+    </div>
+  );
 }
