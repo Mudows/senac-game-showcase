d384c1d4bc39a7bd4657cb57079095dfd9534a6b
Diego Moraes Cezar <dmcezar@gmail.com>
2022-09-19 14:30:31 -0300

    ADD: Base Footer component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Content from './components/Content';
+import Footer from './components/Footer';
 
 import './CSS/App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }
